d4df616a7f00a4340cb3f07eed2a366c9b24f6ad
plammerts <philippelammerts@gmail.com>
2022-12-14 23:20:32 +0100

    Day 13: part 1 attempt

---
+++ 2022/day 13/part 1.py
@@ -0,0 +1,46 @@
+import itertools
+
+
+file = open("data.txt")
+lines = file.read().split('\n')
+pairs = []
+current_pair = []
+for line in lines:
+    if line == "":
+        pairs.append(current_pair)
+        current_pair = []
+    else:
+        line = eval(line)
+        current_pair.append(line)
+
+
+def is_right_order(left, right):
+    if right == []:
+        return False
+    elif left == []:
+        return True
+    elif isinstance(left, list) and isinstance(right, list):
+        for l, r in list(itertools.zip_longest(left, right, fillvalue=[])):
+            res = is_right_order(l, r)
+            if res == None:
+                continue
+            else:
+                return res
+    elif isinstance(left, list):
+        return is_right_order(left, [right])
+    elif isinstance(right, list) and isinstance(left, int):
+        return is_right_order([left], right)
+    elif left > right:
+        return False
+    elif left < right:
+        return True
+    elif left == right:
+        return None
+
+
+sum = 0
+for index, (left, right) in enumerate(pairs):
+    if is_right_order(left, right):
+        print("Correct: ", index + 1)
+        sum += index + 1
+print("Answer: ", sum)
